7f2f46e6865c2cd720edff06ef65ec60b8dc3540
UpalBarua <upalbarua52@gmail.com>
2024-05-10 20:27:19 +0600

    add: navbar

---
+++ client/src/config/index.ts
@@ -0,0 +1,113 @@
+export const mainNavLinks = [
+  {
+    label: "Alert Dialog",
+    href: "/docs/primitives/alert-dialog",
+    description:
+      "A modal dialog that interrupts the user with important content and expects a response.",
+    subLinks: [],
+  },
+  {
+    label: "Hover Card",
+    description:
+      "For sighted users to preview content available behind a link.",
+    subLinks: [
+      {
+        label: "Progress",
+        href: "/docs/primitives/progress",
+        description:
+          "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
+      },
+      {
+        label: "Scroll-area",
+        href: "/docs/primitives/scroll-area",
+        description: "Visually or semantically separates content.",
+      },
+      {
+        label: "Tabs",
+        href: "/docs/primitives/tabs",
+        description:
+          "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
+      },
+      {
+        label: "Tabs",
+        href: "/docs/primitives/tabs",
+        description:
+          "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
+      },
+      {
+        label: "Tooltip",
+        href: "/docs/primitives/tooltip",
+        description:
+          "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
+      },
+    ],
+  },
+  {
+    label: "Hover Card",
+    description:
+      "For sighted users to preview content available behind a link.",
+    subLinks: [
+      {
+        label: "Progress",
+        href: "/docs/primitives/progress",
+        description:
+          "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
+      },
+      {
+        label: "Progress",
+        href: "/docs/primitives/progress",
+        description:
+          "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
+      },
+      {
+        label: "Scroll-area",
+        href: "/docs/primitives/scroll-area",
+        description: "Visually or semantically separates content.",
+      },
+      {
+        label: "Alert Dialog",
+        href: "/docs/primitives/alert-dialog",
+        description:
+          "A modal dialog that interrupts the user with important content and expects a response.",
+        subLinks: [],
+      },
+    ],
+  },
+  {
+    label: "Alert Dialog",
+    href: "/docs/primitives/alert-dialog",
+    description:
+      "A modal dialog that interrupts the user with important content and expects a response.",
+    subLinks: [],
+  },
+  {
+    label: "Hover Card",
+    description:
+      "For sighted users to preview content available behind a link.",
+    subLinks: [
+      {
+        label: "Progress",
+        href: "/docs/primitives/progress",
+        description:
+          "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
+      },
+      {
+        label: "Scroll-area",
+        href: "/docs/primitives/scroll-area",
+        description: "Visually or semantically separates content.",
+      },
+      {
+        label: "Tabs",
+        href: "/docs/primitives/tabs",
+        description:
+          "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
+      },
+      {
+        label: "Tooltip",
+        href: "/docs/primitives/tooltip",
+        description:
+          "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
+      },
+    ],
+  },
+] as const;
