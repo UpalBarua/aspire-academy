8b0bc571d5582a416ecf8383d4bd690e206a8e29
UpalBarua <upalbarua52@gmail.com>
2024-05-10 20:34:22 +0600

    fix: merge conflicts

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,4 +1,4 @@
-import Testimonial from "@/components/testimonial";
+import Counter from "@/components/ui/Counter";
 import { ThemeToggle } from "@/components/ui/theme-toggle";
 import WhatWeProvide from "@/components/what-we-provide";
 
@@ -7,8 +7,8 @@ export default async function Home() {
     <div>
       <h1 className="p-4 text-2xl font-medium">Aspire Academy</h1>
       <ThemeToggle />
-      <WhatWeProvide/>
-      <Counter/>
+      <WhatWeProvide />
+      <Counter />
     </div>
   );
 }
