e748061bf03d95ebe73af8e6a1ff9780c4763650
UpalBarua <upalbarua52@gmail.com>
2024-05-25 17:42:36 +0600

    add: course search

---
+++ client/src/app/(main)/courses/page.tsx
@@ -0,0 +1,40 @@
+import { CourseCard } from "@/components/course-card";
+import { CourseSearch } from "@/components/course-search";
+
+const COURSE = {
+  id: 1,
+  duration: "6h 50m",
+  batch: "Offline",
+  course_name: "Expert in Graphics",
+  details:
+    "It is a course where you learn how you use Graphics fundamental in a project.Explore Real time project Design",
+  instructor: "Shawon Hasan",
+  amount: "5,500",
+  course_banner:
+    "https://images.unsplash.com/photo-1610433572201-110753c6cff9?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  instructor_pic:
+    "https://images.unsplash.com/photo-1566753323558-f4e0952af115?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bWFsZXxlbnwwfHwwfHx8MA%3D%3D",
+};
+
+export default function CoursesPage() {
+  return (
+    <div className="mx-auto max-w-7xl px-2">
+      <div className="relative flex max-w-7xl items-center justify-between px-2 pb-10">
+        <div className="space-y-2">
+          <h2 className="text-3xl font-medium tracking-tight">
+            Courses to get you started
+          </h2>
+          <p className="leading-relaxed text-secondary-foreground">
+            Explore courses from experienced, real-world experts.
+          </p>
+        </div>
+        <CourseSearch />
+      </div>
+      <div className="grid grid-cols-3 gap-4">
+        <CourseCard {...COURSE} />
+        <CourseCard {...COURSE} />
+        <CourseCard {...COURSE} />
+      </div>
+    </div>
+  );
+}
